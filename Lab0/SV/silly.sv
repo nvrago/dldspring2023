6fd87040ed60ea36deade4d13115e854ab7aa9b9
nvrago <110706524+nvrago@users.noreply.github.com>
2023-02-03 16:24:25 -0600

    fixed silly.do file - formatting error

--- Lab0/SV/silly.sv
+++ Lab0/SV/silly.sv
@@ -2,22 +2,23 @@ module silly (input  logic a, b, cin, output logic s, cout);
   // shortcut for terminal: C:\Users\nvrag\OneDrive\Documents\GitHub\dldspring2023\Lab0\SV
   
   /* original text 
-  assign y = ~b & ~c | a & ~b;*/
-  
+  assign y = ~b & ~c | a & ~b;
+  */
+
   /*Logic must produce:
   sum = a ⊕ b ⊕ c_in
-  cout = (a · b) + (a · cin) + (b · cin) */
-  
-   logic p, g; // internal nodes
+  cout = (a · b) + (a · cin) + (b · cin)
+  */
+  logic p, g; // internal nodes
    
-   // main design
-   assign p = a ^ b; 
-   assign g = a & b;
+  // main design
+  assign p = a ^ b; 
+  assign g = a & b;
 
-   assign s = p ^ cin; 
-   assign cout = g | (p & cin);
+  assign s = p ^ cin; 
+  assign cout = g | (p & cin);
     
-   /*alternate design
-   assign cout = (a&b)|(a&cin)|(b&cin);
-   /*
+  //alternate design
+  //assign cout = (a&b)|(a&cin)|(b&cin);
+
 endmodule
